fc034152ff9ed2e9b7b8460bcea663e851ae3780
Steve Mann <stevemann2705@gmail.com>
2019-02-12 23:11:38 +0530

    Initial commit

---
+++ src/main/java/in/stevemann/spring5recipeapp/Spring5RecipeAppApplication.java
@@ -0,0 +1,14 @@
+package in.stevemann.spring5recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5RecipeAppApplication.class, args);
+    }
+
+}
+
