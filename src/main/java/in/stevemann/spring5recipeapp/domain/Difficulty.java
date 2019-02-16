470c23f724b358c96fd275fb10eb1f6497e4222d
Steve Mann <stevemann2705@gmail.com>
2019-02-16 07:45:37 +0530

    Creating Difficulty and Adding it to Recipe

---
+++ src/main/java/in/stevemann/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package in.stevemann.spring5recipeapp.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- src/main/java/in/stevemann/spring5recipeapp/domain/Recipe.java
+++ src/main/java/in/stevemann/spring5recipeapp/domain/Recipe.java
@@ -27,6 +27,9 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     public Long getId() {
         return id;
     }
@@ -105,5 +108,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
