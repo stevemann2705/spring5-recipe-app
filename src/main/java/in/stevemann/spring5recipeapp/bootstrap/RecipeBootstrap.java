88d21d68c5dade72e57474ca359fcc9b29100496
Steve Mann <stevemann2705@gmail.com>
2019-02-21 17:00:42 +0530

    Refactoring Code using Project Lombak

--- src/main/java/in/stevemann/spring5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/in/stevemann/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -4,6 +4,7 @@ import in.stevemann.spring5recipeapp.domain.*;
 import in.stevemann.spring5recipeapp.repositories.CategoryRepository;
 import in.stevemann.spring5recipeapp.repositories.RecipeRepository;
 import in.stevemann.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
     private final CategoryRepository categoryRepository;
@@ -30,6 +32,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         recipeRepository.saveAll(getRecipes());
+        log.debug("Loaded Bootstrap Data");
     }
 
     private List<Recipe> getRecipes(){
--- src/main/java/in/stevemann/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/in/stevemann/spring5recipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package in.stevemann.spring5recipeapp.services;
 
 import in.stevemann.spring5recipeapp.domain.Recipe;
 import in.stevemann.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
@@ -16,6 +18,7 @@ public class RecipeServiceImpl implements RecipeService {
     }
 
     public Set<Recipe> getRecipes(){
+        log.debug("I'm in the Service");
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
