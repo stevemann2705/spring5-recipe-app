d88cac5fdd95dae97e4d43f07c50696e94b4063a
Steve Mann <stevemann2705@gmail.com>
2019-02-22 20:38:45 +0530

    Adding Unit Tests for IndexController

---
+++ src/test/java/in/stevemann/spring5recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,38 @@
+package in.stevemann.spring5recipeapp.controllers;
+
+import in.stevemann.spring5recipeapp.services.RecipeServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class IndexControllerTest {
+
+    private IndexController indexController;
+
+    @Mock
+    private RecipeServiceImpl recipeService;
+
+    @Mock
+    private Model model;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+        assertEquals("index", indexController.getIndexPage(model));
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
