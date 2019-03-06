5e18838534743cb377e699854e7a860e0e5bce9b
Steve Mann <stevemann2705@gmail.com>
2019-03-06 23:07:50 +0530

    Adding Recipe Details Page

--- src/test/java/in/stevemann/spring5recipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/in/stevemann/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -8,9 +8,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
 
 public class RecipeServiceImplTest {
@@ -24,6 +26,22 @@ public class RecipeServiceImplTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipeById() {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> returnedRecipe = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(returnedRecipe);
+
+        Recipe recipeGot = recipeService.findById(1L);
+
+        assertNotNull(recipeGot);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+
     }
 
     @Test
