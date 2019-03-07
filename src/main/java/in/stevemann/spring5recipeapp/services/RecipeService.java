a62340d8a7894b9f83f3b95fc5855771ebd3fe8e
Steve Mann <stevemann2705@gmail.com>
2019-03-07 22:45:47 +0530

    Modifying RecipeService to save RecipeCommand

--- src/main/java/in/stevemann/spring5recipeapp/services/RecipeService.java
+++ src/main/java/in/stevemann/spring5recipeapp/services/RecipeService.java
@@ -1,5 +1,6 @@
 package in.stevemann.spring5recipeapp.services;
 
+import in.stevemann.spring5recipeapp.commands.RecipeCommand;
 import in.stevemann.spring5recipeapp.domain.Recipe;
 
 import java.util.Set;
@@ -8,4 +9,6 @@ public interface RecipeService {
     Set<Recipe> getRecipes();
 
     Recipe findById(Long id);
+
+    RecipeCommand saveRecipeCommand(RecipeCommand recipeCommand);
 }
