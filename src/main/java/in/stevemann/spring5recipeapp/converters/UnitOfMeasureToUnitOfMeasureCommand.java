3be26c271a8fda9fe409151d5dfd857596aae698
Steve Mann <stevemann2705@gmail.com>
2019-03-07 21:14:16 +0530

    Adding More Converters

---
+++ src/main/java/in/stevemann/spring5recipeapp/converters/UnitOfMeasureToUnitOfMeasureCommand.java
@@ -0,0 +1,26 @@
+package in.stevemann.spring5recipeapp.converters;
+
+import in.stevemann.spring5recipeapp.commands.UnitOfMeasureCommand;
+import in.stevemann.spring5recipeapp.domain.UnitOfMeasure;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UnitOfMeasureToUnitOfMeasureCommand implements Converter<UnitOfMeasure, UnitOfMeasureCommand> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasureCommand convert(UnitOfMeasure unitOfMeasure) {
+
+        if (unitOfMeasure != null) {
+            final UnitOfMeasureCommand uomc = new UnitOfMeasureCommand();
+            uomc.setId(unitOfMeasure.getId());
+            uomc.setUom(unitOfMeasure.getUom());
+            return uomc;
+        }
+        return null;
+    }
+}
