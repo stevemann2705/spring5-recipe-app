f93fafad1b09661fc883df54d793b4b7f52f72f1
Steve Mann <stevemann2705@gmail.com>
2019-03-14 05:41:17 +0530

    Adding View Ingredients Option

---
+++ src/test/java/in/stevemann/spring5recipeapp/controllers/IngredientControllerTest.java
@@ -0,0 +1,45 @@
+package in.stevemann.spring5recipeapp.controllers;
+
+import in.stevemann.spring5recipeapp.commands.RecipeCommand;
+import in.stevemann.spring5recipeapp.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class IngredientControllerTest {
+    @Mock
+    RecipeService recipeService;
+
+    IngredientController ingredientController;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        ingredientController = new IngredientController(recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
+    }
+
+    @Test
+    public void listIngredients() throws Exception {
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        mockMvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/list"))
+                .andExpect(model().attributeExists("recipe"));
+
+        verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+}
