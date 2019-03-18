712b1a721abcc37d1aace206bc64475e335df734
Steve Mann <stevemann2705@gmail.com>
2019-03-18 07:20:07 +0530

    Adding Show Ingredients Page

--- src/main/java/in/stevemann/spring5recipeapp/controllers/IngredientController.java
+++ src/main/java/in/stevemann/spring5recipeapp/controllers/IngredientController.java
@@ -1,5 +1,6 @@
 package in.stevemann.spring5recipeapp.controllers;
 
+import in.stevemann.spring5recipeapp.services.IngredientService;
 import in.stevemann.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IngredientController {
     private final RecipeService recipeService;
+    private final IngredientService ingredientService;
 
-    public IngredientController(RecipeService recipeService) {
+    public IngredientController(RecipeService recipeService, IngredientService ingredientService) {
         this.recipeService = recipeService;
+        this.ingredientService = ingredientService;
     }
 
     @RequestMapping("/recipe/{recipeId}/ingredients")
@@ -23,5 +26,12 @@ public class IngredientController {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
 
         return "recipe/ingredient/list";
+    }
+
+    @RequestMapping("recipe/{recipeId}/ingredient/{id}/show")
+    public String showRecipeIngredient(@PathVariable String recipeId,
+                                       @PathVariable String id, Model model) {
+        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
+        return "recipe/ingredient/show";
     }
 }
