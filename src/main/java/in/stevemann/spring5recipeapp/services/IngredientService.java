9813b02b34c0393e5996d7c936a8909d9ef724a0
Steve Mann <stevemann2705@gmail.com>
2019-03-18 07:15:00 +0530

    Adding IngredientService and IngredientServiceImpl

---
+++ src/main/java/in/stevemann/spring5recipeapp/services/IngredientService.java
@@ -0,0 +1,7 @@
+package in.stevemann.spring5recipeapp.services;
+
+import in.stevemann.spring5recipeapp.commands.IngredientCommand;
+
+public interface IngredientService {
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+}
