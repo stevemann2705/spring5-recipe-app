b7671e6b5453365275fc17b92540ba111b140a74
Steve Mann <stevemann2705@gmail.com>
2019-03-21 09:04:21 +0530

    Adding ImageService and Implementation

---
+++ src/main/java/in/stevemann/spring5recipeapp/services/ImageService.java
@@ -0,0 +1,7 @@
+package in.stevemann.spring5recipeapp.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
