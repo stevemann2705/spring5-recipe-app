052b08de9d601e592dcf083a1f44b0034124c07d
Steve Mann <stevemann2705@gmail.com>
2019-03-22 12:16:01 +0530

    Adding Custom Error Page for Status 404

--- src/main/java/in/stevemann/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/in/stevemann/spring5recipeapp/controllers/RecipeController.java
@@ -1,14 +1,14 @@
 package in.stevemann.spring5recipeapp.controllers;
 
 import in.stevemann.spring5recipeapp.commands.RecipeCommand;
+import in.stevemann.spring5recipeapp.exceptions.NotFoundException;
 import in.stevemann.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -62,5 +62,18 @@ public class RecipeController {
         recipeService.deleteById(Long.valueOf(id));
 
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
